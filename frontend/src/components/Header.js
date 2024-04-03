776d18e0bf7bc64a3f6ff1f3b9979b32c5e8a3d5
miha11-hash <d_mihaela@msn.com>
2024-04-03 08:32:19 +0300

    custom bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Navbar, Nav, Container} from "react-bootstrap";
 import {FaShoppingCart, FaUser} from "react-icons/fa";
+import logo from "../assets/logo.png"
 
 function Header() {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect className="py-3">
           <Container>
-            <Navbar.Brand href="/">ProShop</Navbar.Brand>
+            <Navbar.Brand href="/"><img src={logo} alt="logo"></img>ProShop</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           </Container>
           <Navbar.Collapse id="basic-navbar-nav">
