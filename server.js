53ed8d3967de2e8bbd57c69a987caab2b26aa4de
miha11-hash <d_mihaela@msn.com>
2024-04-17 22:38:36 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+import {products} from "./data/dataProducts.js";
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+})
+
+app.get("/api/products", (req,res) => {
+    res.json(products)
+})
+
+app.get("/api/v1/products/:id", (req, res) => {
+    const product=products.find(p => p._id === req.params.id);
+    res.json(product)
+})
+
+const port=4000;
+
+app.listen(port, () => {
+console.log(`Server is listening at ${port}`)
+})
